bb37741fc08cb4086c037d9a3e53a298edfea55a
Indiamara Colla <maratsiqueira@gmail.com>
2020-05-16 19:03:43 -0300

    Adicionando assets ao projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import StatusBarApp from '~/atoms/StatusBar'
+import Main from './navigations/Main'
+
+const App = () => (
+  <>
+    <StatusBarApp />
+    <Main />
+  </>
+)
+
+export default App
